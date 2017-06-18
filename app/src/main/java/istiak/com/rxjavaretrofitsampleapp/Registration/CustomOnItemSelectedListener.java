e53af8c52a49f96ef01fcfef54d972f382abff9f
Istiak morsalin <anik.cse@gmail.com>
2017-06-18 13:42:00 +0600

    UI related changes

---
+++ app/src/main/java/istiak/com/rxjavaretrofitsampleapp/Registration/CustomOnItemSelectedListener.java
@@ -0,0 +1,25 @@
+package istiak.com.rxjavaretrofitsampleapp.Registration;
+
+/**
+ * Created by slbd on 6/18/17.
+ */
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+        Toast.makeText(parent.getContext(),
+                "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+}
