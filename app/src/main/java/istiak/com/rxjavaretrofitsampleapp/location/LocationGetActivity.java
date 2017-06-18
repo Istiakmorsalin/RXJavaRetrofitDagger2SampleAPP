6955254f6df27338ffa4517806b118b0fa2460eb
Istiak morsalin <anik.cse@gmail.com>
2017-06-18 13:45:23 +0600

    location get activity initiate

---
+++ app/src/main/java/istiak/com/rxjavaretrofitsampleapp/location/LocationGetActivity.java
@@ -0,0 +1,23 @@
+package istiak.com.rxjavaretrofitsampleapp.location;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import butterknife.ButterKnife;
+import istiak.com.rxjavaretrofitsampleapp.R;
+import istiak.com.rxjavaretrofitsampleapp.RXJavaRetrofitSampleApp;
+
+/**
+ * Created by slbd on 6/18/17.
+ */
+
+public class LocationGetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
